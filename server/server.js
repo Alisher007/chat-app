9657f10ad60419a94ad09364c1f3fccfbf4ed44d
Alisher Khalikulov <alisher.khalikulov@jaresorts.com>
2018-05-26 12:23:16 +0400

    Greet new user

--- server/server.js
+++ server/server.js
@@ -12,6 +12,17 @@ var io = socketIO(server);
 io.on('connection',(socket) => {
   console.log('new user connected');
 
+  socket.emit('newMessage',{
+    from: 'admin',
+    text: 'welcome',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage',{
+    from: 'admin',
+    text: 'new user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage',function (message) {
     console.log('createMessage',message);
     io.emit('newMessage',{
@@ -19,6 +30,11 @@ io.on('connection',(socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage',{
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
   socket.on('disconnect',() => {
     console.log('user was disconnected');
