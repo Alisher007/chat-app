e7663d71afdebbe7cd97c86d90663b17da0cf3d7
Alisher Khalikulov <alisher.khalikulov@jaresorts.com>
2018-05-26 12:03:40 +0400

    Emit newMessage

--- server/server.js
+++ server/server.js
@@ -12,13 +12,13 @@ var io = socketIO(server);
 io.on('connection',(socket) => {
   console.log('new user connected');
 
-  socket.emit('newMessage',{
-    from: 'John',
-    text: 'Hey, newMessage',
-    createdAt: 123
-  });
   socket.on('createMessage',function (message) {
     console.log('createMessage',message);
+    io.emit('newMessage',{
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
   socket.on('disconnect',() => {
     console.log('user was disconnected');
